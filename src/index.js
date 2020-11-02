564b81a81f84a8a21a7ff6eea5c5ef20241761f4
Ben Rowe <ben.rowe.83@gmail.com>
2020-11-03 10:52:22 +1100

    Setup basic index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-var Botkit = require('botkit');
+'use strict'
 
-var controller = Botkit.slackbot();
-
-var bot = controller.spawn({
-
-  token: "<your token>"
-
-})
+exports.handler = function (event, context, callback) {
+  var response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html; charset=utf-8',
+    },
+    body: '<p>Hello world!</p>',
+  }
+  callback(null, response)
+}
