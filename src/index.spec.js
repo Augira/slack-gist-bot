99789a7a57069beddcde6d1c9f093c411c0ddd2c
Ben Rowe <ben.rowe.83@gmail.com>
2020-11-09 07:35:20 +1100

    Add tests

---
+++ src/index.spec.js
@@ -0,0 +1,9 @@
+const handle = require('./index');
+
+describe('Entrypoint', () => {
+  it('returns with json', () => {
+    handle.handler({}, {}, (tmp, req) => {
+      expect(req.body).toBe('{"text": "Thanks"}');
+    })
+  });
+});
