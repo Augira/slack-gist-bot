d1349e7e04df60cb218be1ce9ff9de6508069cf6
Ben Rowe <ben.rowe.83@gmail.com>
2020-11-08 22:01:50 +1100

    Provide terraform root config

--- deploy/terraform/provider.tf
+++ deploy/terraform/provider.tf
@@ -4,3 +4,12 @@ provider "aws" {
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
 }
+
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+    }
+  }
+}
