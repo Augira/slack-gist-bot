8b23876796fc705723ca1658ff6f9da516ab8ca9
Ben Rowe <ben.rowe.83@gmail.com>
2020-11-09 08:53:54 +1100

    Fix lint issues

--- src/index.spec.js
+++ src/index.spec.js
@@ -6,6 +6,6 @@ describe('Entrypoint', () => {
   it('returns with json', () => {
     handle.handler({}, {}, (tmp, req) => {
       expect(req.body).toBe('{"text": "Thanks!"}');
-    })
+    });
   });
 });
